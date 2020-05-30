e21de90c7f415fc336073dc96e46fd8f3a2a792c
GreciaGA <grecia.anicama@gmail.com>
2020-05-30 14:40:59 -0500

    subiendo vista del registro

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
+// Botones ==========================================
 const btnYura = document.getElementById('btnYura');
+const btnIngresar = document.getElementById('ingresar');
+const btnRegistrarme = document.getElementById('registrarme');
+
+// Boton para ingresar a App
+const btnguardarDatos = document.getElementById('guardar-datos');
+const ingresarBtnIngresar =document.getElementById('ingresar-btnIngresar');
+
+// Elementos del DOM ================================
 const container1 = document.getElementById('container1');
 const container2 = document.getElementById('container2');
-
+const registrarmeForm= document.getElementById('registrarme-form');
+const ingresarForm = document.getElementById('ingresar-form');
 
 btnYura.addEventListener('click', ()=>{
     container1.classList.add("hide");
     container1.classList.remove("show");
     container1.classList.remove("container1");
     container2.classList.add("hide");
-    // container2.classList.remove("container1");
-
-
-
-    console.log("img hide");
-    
     container2.classList.replace("hide","show");
-    console.log("algo");
-    
 })
+
+btnRegistrarme.addEventListener('click',()=>{
+    container1.classList.add("hide");
+    container2.classList.add("hide");
+    btnIngresar.classList.add("hide");
+    registrarmeForm.classList.replace("hide","show");
+})
+
+btnIngresar.addEventListener('click',()=>{
+    container1.classList.add("hide");
+    container2.classList.add("hide");
+    btnIngresar.classList.add("hide");
+    ingresarForm.classList.replace('hide','show');
+
+})
+
